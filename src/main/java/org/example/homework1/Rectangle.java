15c21625b85919bf3093a0e463a634e1fbca55ee
Irasp <iraspjob@gmail.com>
2023-04-03 22:09:10 +0300

    The first commit

---
+++ src/main/java/org/example/homework1/Rectangle.java
@@ -0,0 +1,31 @@
+package org.example.homework1;
+
+public class Rectangle {
+
+    private int pointSe;
+    private int pointNw;
+
+    public Rectangle(int pointSe, int pointNw) {
+        this.pointSe = pointSe;
+        this.pointNw = pointNw;
+    }
+
+    public int getPointSe() {
+        return pointSe;
+    }
+
+    public void setPointSe(int pointSe) {
+        this.pointSe = pointSe;
+    }
+
+    public int getPointNw() {
+        return pointNw;
+    }
+
+    public void setPointNw(int pointNw) {
+        this.pointNw = pointNw;
+    }
+    double area() {
+        return area();
+    }
+}
